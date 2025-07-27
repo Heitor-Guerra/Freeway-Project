663a1d01abb45e0b56ac1437b06ca0176cc560db
Heitor Guerra <hgproveti@gmail.com>
2025-07-27 09:52:39 -0300

    On branch master  Your branch is up to date with 'origin/master'.
     Tipos de Dados definidos pelo usuario
    
     Changes to be committed:
    	modified:   main.c

--- main.c
+++ main.c
@@ -1,4 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/*-------------------Constantes-------------------*/
+
+
+#define MAX_CARROS 10
+#define MAX_PISTAS 12
+#define MAX_LARGURA 100
+#define MAX_ALTURA 35
+#define MAX_VIDAS 3
+
+
+/*------------Tipos de Dados Definidos------------*/
+
+
+typedef struct {
+    int x;
+    int y;
+    int vidas;
+    char desenho[2][4]; //2 linhas e 3 caracteres + \0
+} tGalinha;
+
+typedef struct {
+    int id;
+    int x;
+    char desenho[4][2][4]; //4, animacoes, 2 linhas e 3 caracteres + \0
+} tCarro;
+
+typedef struct {
+    int id;
+    int velocidade;
+    int numCarros;
+    char direcao;
+    tCarro carros[MAX_CARROS];
+} tPista;
+
+typedef struct {
+    int idPista;
+    int idCarro;
+    int iteracao;
+} tAtropelamentos;  //Importante para gerar o resumo e ranking
+
+typedef struct {
+    tGalinha galinha;
+    tPista pistas[MAX_PISTAS];
+    tAtropelamentos atropelamentos[MAX_VIDAS];
+    int quantidadePistas;
+    int largura;
+    char grade[2][MAX_ALTURA][MAX_LARGURA]; //ACHO que e necessario o 2 para o heatmap
+} tJogo;
+
+
+/*------------Funcoes XXXXXX------------*/
+
+
 
 int main() {
     int a;
