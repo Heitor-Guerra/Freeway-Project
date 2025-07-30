ba75978b5225bf35dd71d6b896b6b6767fc1afde
Heitor Guerra <hgproveti@gmail.com>
2025-07-30 15:17:39 -0300

    Jogo Pronto Falta: Inicializacao, resumo, estatistica, ranking e heatmap  On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   freeway
    	new file:   main
    	modified:   main.c

--- main.c
+++ main.c
@@ -10,7 +10,6 @@
 #define MAX_PISTAS 12
 #define MAX_LARGURA 102      //largura + 2 para laterais
 #define MAX_ALTURA 37        //altura + 2 para borda superior e inferior
-#define MAX_VIDAS 3
 
 
 /*-----------------------------------------Tipos de Dados Definidos-----------------------------------------*/
@@ -35,6 +34,8 @@ typedef struct {
     int idPista;
     int idCarro;
     int iteracao;
+    int x;
+    int y;
 } tAtropelamentos;              //Importante para gerar o resumo e ranking
 
 typedef struct {
@@ -45,12 +46,15 @@ typedef struct {
     char grade[MAX_ALTURA][MAX_LARGURA]; 
     tGalinha galinha;
     tPista pistas[MAX_PISTAS];    // o id da pista e a posicao no vetor
-    tAtropelamentos atropelamentos[MAX_VIDAS];
 } tJogo;
 
 
 /*---------------------------------------------Funcoes Galinha----------------------------------------------*/
 
+
+tGalinha InicializaGalinha() {
+
+}
 
 tGalinha MovimentaGalinha(tGalinha galinha, char mov, int qtdPistas) {
     if(mov == 'w') {
@@ -92,23 +96,29 @@ void DesenhaGalinha(tGalinha galinha, char mapa[MAX_ALTURA][MAX_LARGURA]) {
 /*----------------------------------------------Funcoes Pista-----------------------------------------------*/
 
 
+tPista InicializaPista(){
+
+}
+
 tPista MovimentaCarrosNaPista(tPista pista, int largura) {
     int i;
-    if(pista.direcao == 'D') {
-        for(i = 0; i < pista.numCarros; i++) {
-            pista.carrosX[i] += pista.velocidade; //Move para direita
-            if(pista.carrosX[i] > largura) {
-                pista.carrosX[i] -= largura;      //Aparece do outro lado ao ultrapassar a borda
+    switch (pista.direcao) {
+        case 'D':
+            for(i = 0; i < pista.numCarros; i++) {
+                pista.carrosX[i] += pista.velocidade; //Move para direita
+                if(pista.carrosX[i] > largura) {
+                    pista.carrosX[i] -= largura;      //Aparece do outro lado ao ultrapassar a borda
+                }
             }
-        }
-    }
-    else if(pista.direcao == 'E') {
-        for(i = 0; i < pista.numCarros; i++) {
-            pista.carrosX[i] -= pista.velocidade;  //Move para esquerda
-            if(pista.carrosX[i] < 1) {
-                pista.carrosX[i] += largura;       //Aparece do outro lado ao ultrapassar a borda
+            break;
+        case 'E':
+            for(i = 0; i < pista.numCarros; i++) {
+                pista.carrosX[i] -= pista.velocidade;  //Move para esquerda
+                if(pista.carrosX[i] < 1) {
+                    pista.carrosX[i] += largura;       //Aparece do outro lado ao ultrapassar a borda
+                }
             }
-        }
+            break;
     }
     return pista;
 }
@@ -169,8 +179,90 @@ void DesenhaCarrosNaPista(tPista pista, char mapa[MAX_ALTURA][MAX_LARGURA], int 
 }
 
 
+/*-------------------------------------------Funcoes Atropelamento------------------------------------------*/
+
+
+tAtropelamentos AtribuiInfo(int idPista, int idCarro, int iteracao, int x, int y) {
+    tAtropelamentos atropelamento;
+    atropelamento.idPista = idPista;
+    atropelamento.idCarro = idCarro;
+    atropelamento.iteracao = iteracao;
+    atropelamento.x = x;
+    atropelamento.y = y;
+    return atropelamento;
+}
+
+void TrocaPosicaoVetor(tAtropelamentos vet[], int pos1, int pos2) {
+    tAtropelamentos aux;
+    aux = vet[pos1];
+    vet[pos1] = vet[pos2];
+    vet[pos2] = aux;
+}
+//Nao testado
+void OrdenaAtropelamentos(tAtropelamentos atropelamentos[], int numAtropelamentos) { //Ordenacao BubbleSort
+    int i, pronto = 0;        
+    while(!pronto) {
+        for(i = 1; i < numAtropelamentos; i++) {
+            if(atropelamentos[i-1].idPista > atropelamentos[i].idPista) {
+                TrocaPosicaoVetor(atropelamentos, i-1, i);
+            }
+            else if(atropelamentos[i-1].idPista == atropelamentos[i].idPista) {
+                if(atropelamentos[i-1].idCarro > atropelamentos[i].idCarro) {
+                    TrocaPosicaoVetor(atropelamentos, i-1, i);
+                }
+                else if((atropelamentos[i-1].idCarro == atropelamentos[i].idCarro) && atropelamentos[i-1].iteracao > atropelamentos[i].iteracao) {
+                    TrocaPosicaoVetor(atropelamentos, i-1, i);
+                }
+            }
+        }
+        pronto = 1;
+        for(i = 1; i < numAtropelamentos; i++) {
+            if(atropelamentos[i-1].idPista > atropelamentos[i].idPista) {
+                pronto = 0;
+            }
+            else if(atropelamentos[i-1].idPista == atropelamentos[i].idPista) {
+                if(atropelamentos[i-1].idCarro > atropelamentos[i].idCarro) {
+                    pronto = 0;
+                }
+                else if((atropelamentos[i-1].idCarro == atropelamentos[i].idCarro) && atropelamentos[i-1].iteracao > atropelamentos[i].iteracao) {
+                    pronto = 0;
+                }
+            }
+        }
+    }
+}
+
+
 /*-----------------------------------------------Funcoes Jogo-----------------------------------------------*/
 
+
+tJogo InicializaMapaJogo(tJogo jogo) {
+    int i, j;
+    for(i = 0; i < jogo.quantidadePistas*3 + 1; i++) {
+        for(j = 0; j < jogo.largura + 2; j++) {
+            if(j == 0 || j == jogo.largura + 1) {      //Desenha as laterais
+                jogo.grade[i][j] = '|';
+            }
+            else if(i%3 == 0) {                    //Desenha as divisorias de pista
+                if(j%3) {
+                    jogo.grade[i][j] = '-';
+                }
+                else {
+                    jogo.grade[i][j] = ' ';
+                }
+            }
+            else {                                 //Desenha os espacoes na pista
+                jogo.grade[i][j] = ' ';
+            }
+        }
+    }
+    return jogo;
+}
+
+tJogo InicializaJogo() {
+
+
+}
 
 int AcabouJogo(tJogo jogo) {
     if(ObtemVidasGalinha(jogo.galinha) == 0 || ObtemYGalinha(jogo.galinha) == 1) {
@@ -179,37 +271,48 @@ int AcabouJogo(tJogo jogo) {
     return 0;
 }
 
-void DesenhaMapaJogo(tJogo jogo) {  //Desenha o mapa, printa, e apaga em sequencia            ////Possivel erro
+void DesenhaMapaJogo(tJogo jogo, int pontos) {  //Desenha e printa o mapa  //Como a funcao e void nao causa alteracao permanente no grid
     int i, j;
+
     DesenhaGalinha(jogo.galinha, jogo.grade);
     for(i = 0; i < jogo.quantidadePistas; i++) {
         DesenhaCarrosNaPista(jogo.pistas[i], jogo.grade, i, jogo.largura, jogo.animacoes, jogo.iteracao);
     }
+
+    printf("Pontos: %d | Vidas: %d | Iteracoes: %d\n", pontos, ObtemVidasGalinha(jogo.galinha), jogo.iteracao);
     for(i = 0; i < jogo.quantidadePistas*3 + 1; i++) {       //printa o grid
         for(j = 0; j < jogo.largura + 2; j++) {              //largura + 2 para correcao da borda
             printf("%c", jogo.grade[i][j]);
         }
         printf("\n");
     }
-    for(i = 1; i < jogo.quantidadePistas*3 + 1; i++) {       //apaga as alteracoes
-        for(j = 1; j < jogo.largura + 1; j++) {
-            if(i%3) {
-                jogo.grade[i][j] = ' ';              /////////////////////Possivel erro
-            }
-        }
-    }
 }
 
 void JogaJogo(tJogo jogo) {
-    int i;
-    int pistaGalinha;
+    int i, pistaGalinha, carroAtr;
+    int pontos = 0;
     char movimento = 0;
 
+    int numMov = 0, numMovS = 0, yMaxGalinha = 0, yMaxAtr = 0, yMinAtr = 0;
+    int numAtr = 0;                                                             //Variaveis para estatistica
+    tAtropelamentos atropelamentos[ObtemVidasGalinha(jogo.galinha)];
 
+    DesenhaMapaJogo(jogo, pontos);
     while(!AcabouJogo(jogo)) {
+
+        /*------------------------Movimento da Galinha e Carros------------------------*/
         scanf("%c", &movimento);
         if(movimento != 'w' && movimento != 's' && movimento != ' ') {
             continue;
+        }
+        switch(movimento) {
+            case 'w':
+                numMov++;
+                break;
+            case 's':
+                numMov++;
+                numMovS++;
+                break;
         }
         jogo.galinha = MovimentaGalinha(jogo.galinha, movimento, jogo.quantidadePistas);
         for(i = 0; i < jogo.quantidadePistas; i++) {
@@ -217,63 +320,49 @@ void JogaJogo(tJogo jogo) {
         }
         pistaGalinha = (ObtemYGalinha(jogo.galinha) - 1)/3;
 
-        if(VerificaColisaoNaPista(jogo.pistas[pistaGalinha], jogo.galinha, jogo.largura)) {
+        /*-------------------------------Atropelamentos-------------------------------*/
+        carroAtr = VerificaColisaoNaPista(jogo.pistas[pistaGalinha], jogo.galinha, jogo.largura);
+        if(carroAtr) {
+            atropelamentos[numAtr] = AtribuiInfo(pistaGalinha+1, carroAtr, jogo.iteracao, ObtemXGalinha(jogo.galinha), ObtemYGalinha(jogo.galinha));
             jogo.galinha = ResetaGalinha(jogo.galinha, jogo.quantidadePistas);
+            pontos = 0;
+            numAtr++;
             if(jogo.animacoes) {
                 jogo.pistas[pistaGalinha] = ReduzVelocidadePista(jogo.pistas[pistaGalinha]);
             }
         }
+        else if (!VerificaColisaoNaPista(jogo.pistas[pistaGalinha], jogo.galinha, jogo.largura) && movimento == 'w') {
+            pontos++;
+        }
 
+
+        /*---------------------------Print do Mapa e outros---------------------------*/
+        if(ObtemYGalinha(jogo.galinha) == 1) {
+            pontos += 10;
+        }
         jogo.iteracao++;
-        printf("VIDAS:%d, ITERACAO:%d\n\n", jogo.galinha.vidas, jogo.iteracao);
-        DesenhaMapaJogo(jogo);
+        DesenhaMapaJogo(jogo, pontos);
     }
+
+    if(ObtemYGalinha(jogo.galinha) == 1) {
+        printf("Parabens! Voce atravessou todas as pistas e venceu!\n");
+    }
+    else {
+        printf("Voce perdeu todas as vidas! Fim de jogo.\n");
+    }
+
 }
 
 
 /*---------------------------------------------------MAIN---------------------------------------------------*/
 
 
-int main() {
+tJogo jogo;        //Struct muito grande, declarada global para nao ocorrer falha de segmentacao
+int main() {       //Nao atrapalha outras funcoes, pois foi declarada no fim do codigo
     int i, j;
 
 
-    tJogo jogo;
-    jogo.largura = 25;
-    jogo.animacoes = 1;
-    jogo.quantidadePistas = 4;
-    jogo.iteracao = -1;
-    jogo.pistas[1].velocidade = 2;
-    jogo.pistas[1].direcao = 'D';
-    jogo.pistas[1].numCarros = 2;
-    jogo.pistas[1].carrosX[0] = 2;
-    jogo.pistas[1].carrosX[1] = 9;
-    jogo.pistas[0].numCarros = 0;
-    jogo.pistas[2].numCarros = 0;
-    jogo.pistas[3].numCarros = 0;
-
-
-    strcpy(jogo.galinha.desenhoGalinha[0], "^_^");
-    strcpy(jogo.galinha.desenhoGalinha[1], "(o)");
-    jogo.galinha.vidas = 2;
-    jogo.galinha.x = 12;
-    jogo.galinha.y = 10;
-
-    strcpy(jogo.pistas[1].desenhoCarro[0][0], "[-]");
-    strcpy(jogo.pistas[1].desenhoCarro[0][1], "---");
-    strcpy(jogo.pistas[1].desenhoCarro[1][0], "[-]");
-    strcpy(jogo.pistas[1].desenhoCarro[1][1], "/-/");
-    strcpy(jogo.pistas[1].desenhoCarro[2][0], "[-]");
-    strcpy(jogo.pistas[1].desenhoCarro[2][1], "|-|");
-    strcpy(jogo.pistas[1].desenhoCarro[3][0], "[-]");
-    strcpy(jogo.pistas[1].desenhoCarro[3][1], "\\-\\");
-
-    for(i  = 0; i < jogo.quantidadePistas*3+1; i++) {
-        for(j = 0; j < jogo.largura+2; j++) {
-            jogo.grade[i][j] = '0';
-        }
-    }
-
+    jogo = InicializaMapaJogo(jogo);
     JogaJogo(jogo);
 
     return 0;
